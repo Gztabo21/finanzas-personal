8c1fdc7605e8045fbf046a7ac543b1b2d92d843e
gustavo <gustavocacharuco@gmail.com>
2021-03-26 15:35:59 -0400

    style en bar

--- src/app/components/product/product.module.ts
+++ src/app/components/product/product.module.ts
@@ -7,7 +7,8 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductListComponent } from './product-list/product-list.component';
 // services
-import { ProductService } from 'src/app/core/service/product.service'
+import { ProductService } from 'src/app/core/service/product.service';
+import { LayoutModule } from 'src/app/components/layout/layout.module';
 
 @NgModule({
   imports: [ 
@@ -15,7 +16,8 @@ import { ProductService } from 'src/app/core/service/product.service'
     FormsModule,
     ReactiveFormsModule,
     ProductRoutingModule,
-    SharedModule, 
+    SharedModule,
+    LayoutModule, 
     IonicModule],
   declarations: [
     ProductFormComponent,
